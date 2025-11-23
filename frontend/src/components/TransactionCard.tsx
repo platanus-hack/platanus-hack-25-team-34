b7e553281cb3e49c9fbf9199b0142c1ed290ccbf
AlonsoUtreras <alonso.utreras@clarovtr.cl>
2025-11-22 21:05:49 -0300

    Delete up/down arrows on the number input fiels in deposit and withdraw cards

--- frontend/src/components/TransactionCard.tsx
+++ frontend/src/components/TransactionCard.tsx
@@ -66,7 +66,18 @@ const TransactionCard: React.FC<TransactionCardProps> = ({
             '& .MuiOutlinedInput-root': {
               borderRadius: '8px',
               backgroundColor: '#f8f9fa'
-            }
+            },
+            '& input[type=number]': {
+              MozAppearance: 'textfield',
+            },
+            '& input[type=number]::-webkit-outer-spin-button': {
+              WebkitAppearance: 'none',
+              margin: 0,
+            },
+            '& input[type=number]::-webkit-inner-spin-button': {
+              WebkitAppearance: 'none',
+              margin: 0,
+            },
           }}
           slotProps={{
              htmlInput: { min: 0 }
